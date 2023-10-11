717b38f7ee1c36a678c2fb7e82db2ef90ccdab88
LadyK <LadyK@users.noreply.github.com>
2023-10-11 15:03:43 -0400

    first push

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+}
+
+function draw() {
+  // put drawing code here
+  background(0);
+  fill(255, 0, 0);
+  noStroke();
+  ellipse(mouseX, mouseY, 50, 50);
+}
